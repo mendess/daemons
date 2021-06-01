b253db1762a69849654033741d8ae9a0abb738fc
mendess <pedro.mendes.26@gmail.com>
2021-06-02 00:47:03 +0100

    First version

---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+#![deny(unused_crate_dependencies)]
+#![deny(rust_2018_idioms)]
+
+use std::{
+    collections::HashMap,
+    error::Error,
+    sync::Arc,
+    time::{Duration, Instant},
+};
+use tokio::{
+    sync::mpsc::{self, Sender},
+    time::timeout,
+};
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ControlFlow {
+    Continue,
+    Break,
+}
+
+impl ControlFlow {
+    pub fn must_break(self) -> bool {
+        self == ControlFlow::Break
+    }
+
+    pub fn is_continue(self) -> bool {
+        !self.must_break()
+    }
+}
+
+/// A Daemon
+#[async_trait::async_trait]
+pub trait Daemon {
+    type Data;
+
+    /// What the daemon does when it runs
+    async fn run(&self, data: &Self::Data) -> ControlFlow;
+    /// How frequent the daemon must run
+    fn interval(&self) -> Duration;
+    /// The name of the daemon
+    fn name(&self) -> String;
+}
+
+pub struct DaemonThread<Data> {
+    next_id: usize,
+    channels: HashMap<usize, (String, Sender<()>)>,
+    data: Arc<Data>,
+}
+
+impl<D: Send + Sync + 'static> DaemonThread<D> {
+    pub async fn run_one(&mut self, i: usize) -> Result<(), usize> {
+        use futures::future::OptionFuture as OptFut;
+        match OptFut::from(self.channels.get(&i).map(|(_, c)| c.send(()))).await {
+            Some(Ok(_)) => Ok(()),
+            _ => Err(i),
+        }
+    }
+
+    pub async fn run_all(&mut self) -> Result<(), usize> {
+        for (i, (_, c)) in self.channels.iter() {
+            c.send(()).await.map_err(|_| *i)?
+        }
+        Ok(())
+    }
+
+    pub async fn add_daemon<E, T>(&mut self, daemon: T) -> usize
+    where
+        E: Error + 'static,
+        T: Daemon<Data = D> + Send + Sync + 'static,
+    {
+        let id = self.next_id;
+        let (sx, mut rx) = mpsc::channel(10);
+        self.channels.insert(id, (daemon.name(), sx));
+        let data = self.data.clone();
+        tokio::spawn(async move {
+            loop {
+                let now = Instant::now();
+                let next_run = now + daemon.interval();
+                let _ = timeout(next_run - now, rx.recv()).await;
+
+                if daemon.run(&data).await.must_break() {
+                    break;
+                }
+            }
+        });
+        self.next_id += 1;
+        id
+    }
+
+    pub fn daemon_names(&self) -> impl Iterator<Item = &str> {
+        self.channels.values().map(|(n, _)| n.as_str())
+    }
+
+    pub fn start(data: Arc<D>) -> Self {
+        data.into()
+    }
+}
+
+impl<D: Send + Sync + 'static> From<Arc<D>> for DaemonThread<D> {
+    fn from(data: Arc<D>) -> Self {
+        Self {
+            next_id: 0,
+            channels: HashMap::new(),
+            data,
+        }
+    }
+}
