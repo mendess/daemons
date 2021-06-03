a9c681c7931f3530ff369586111196d0eb30829c
mendess <pedro.mendes.26@gmail.com>
2021-06-03 05:07:46 +0100

    Add cancelability

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@
 #![deny(unsafe_code)]
 #![deny(unused_must_use)]
 
+use futures::future::OptionFuture as OptFut;
 use std::{
     collections::HashMap,
+    num::Wrapping,
     sync::Arc,
     time::{Duration, Instant},
 };
@@ -23,12 +25,12 @@ pub enum ControlFlow {
 }
 
 impl ControlFlow {
-    pub fn must_break(self) -> bool {
+    pub fn is_break(self) -> bool {
         self == ControlFlow::Break
     }
 
     pub fn is_continue(self) -> bool {
-        !self.must_break()
+        !self.is_break()
     }
 }
 
@@ -45,16 +47,20 @@ pub trait Daemon {
     async fn name(&self) -> String;
 }
 
+enum Msg {
+    Run,
+    Cancel,
+}
+
 pub struct DaemonThread<Data> {
-    next_id: usize,
-    channels: HashMap<usize, (String, Sender<()>)>,
+    next_id: Wrapping<usize>,
+    channels: HashMap<usize, (String, Sender<Msg>)>,
     data: Arc<Data>,
 }
 
 impl<D: Send + Sync + 'static> DaemonThread<D> {
     pub async fn run_one(&mut self, i: usize) -> Result<(), usize> {
-        use futures::future::OptionFuture as OptFut;
-        match OptFut::from(self.channels.get(&i).map(|(_, c)| c.send(()))).await {
+        match OptFut::from(self.channels.get(&i).map(|(_, c)| c.send(Msg::Run))).await {
             Some(Ok(_)) => Ok(()),
             _ => Err(i),
         }
@@ -62,9 +68,17 @@ impl<D: Send + Sync + 'static> DaemonThread<D> {
 
     pub async fn run_all(&mut self) -> Result<(), usize> {
         for (i, (_, c)) in self.channels.iter() {
-            c.send(()).await.map_err(|_| *i)?
+            c.send(Msg::Run).await.map_err(|_| *i)?
         }
         Ok(())
+    }
+
+    pub async fn cancel(&mut self, i: usize) -> Result<(), usize> {
+        if let None = OptFut::from(self.channels.get(&i).map(|(_, c)| c.send(Msg::Cancel))).await {
+            Err(i)
+        } else {
+            Ok(())
+        }
     }
 
     pub async fn add_daemon<T>(&mut self, mut daemon: T) -> usize
@@ -73,21 +87,25 @@ impl<D: Send + Sync + 'static> DaemonThread<D> {
     {
         let id = self.next_id;
         let (sx, mut rx) = mpsc::channel(10);
-        self.channels.insert(id, (daemon.name().await, sx));
+        self.channels.insert(id.0, (daemon.name().await, sx));
         let data = self.data.clone();
         tokio::spawn(async move {
             loop {
                 let now = Instant::now();
                 let next_run = now + daemon.interval().await;
-                let _ = timeout(next_run - now, rx.recv()).await;
+                //TODO: or patterns
+                if let Ok(Some(Msg::Cancel)) | Ok(None) = timeout(next_run - now, rx.recv()).await
+                {
+                    break;
+                }
 
-                if daemon.run(&data).await.must_break() {
+                if daemon.run(&data).await.is_break() {
                     break;
                 }
             }
         });
-        self.next_id += 1;
-        id
+        self.next_id += Wrapping(1);
+        id.0
     }
 
     pub async fn add_shared<T>(&mut self, daemon: Arc<Mutex<T>>) -> usize
@@ -97,7 +115,7 @@ impl<D: Send + Sync + 'static> DaemonThread<D> {
         let id = self.next_id;
         let (sx, mut rx) = mpsc::channel(10);
         self.channels
-            .insert(id, (daemon.lock().await.name().await, sx));
+            .insert(id.0, (daemon.lock().await.name().await, sx));
         let data = self.data.clone();
         tokio::spawn(async move {
             loop {
@@ -105,13 +123,13 @@ impl<D: Send + Sync + 'static> DaemonThread<D> {
                 let next_run = now + daemon.lock().await.interval().await;
                 let _ = timeout(next_run - now, rx.recv()).await;
 
-                if daemon.lock().await.run(&data).await.must_break() {
+                if daemon.lock().await.run(&data).await.is_break() {
                     break;
                 }
             }
         });
-        self.next_id += 1;
-        id
+        self.next_id += Wrapping(1);
+        id.0
     }
 
     pub fn daemon_names(&self) -> impl Iterator<Item = (usize, &str)> {
@@ -126,7 +144,7 @@ impl<D: Send + Sync + 'static> DaemonThread<D> {
 impl<D: Send + Sync + 'static> From<Arc<D>> for DaemonThread<D> {
     fn from(data: Arc<D>) -> Self {
         Self {
-            next_id: 0,
+            next_id: Wrapping(0),
             channels: HashMap::new(),
             data,
         }
